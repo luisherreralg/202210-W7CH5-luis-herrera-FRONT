68065c46e3d826cfa6eb5d45751667de6360b408
luisherreralg <lherreralg@outlook.es>
2022-11-20 22:20:19 +0100

    💄 Added basic css

--- src/infrastructure/componentes/app/app.tsx
+++ src/infrastructure/componentes/app/app.tsx
@@ -5,9 +5,13 @@ import './app.css';
 export function App() {
     return (
         <>
-            <div className="app">Estoy usando la plantilla de LUIS</div>
-            <RobotList></RobotList>
-            <AddForm></AddForm>
+            <main className="bg-slate-900 text-gray-200 flex flex-col items-center font-mono">
+                <div className="flex justify-center w-1/2 m-10 rounded-2xl">
+                    <h1 className="text-6xl p-40">Robots</h1>
+                </div>
+                <RobotList></RobotList>
+                <AddForm></AddForm>
+            </main>
         </>
     );
 }
