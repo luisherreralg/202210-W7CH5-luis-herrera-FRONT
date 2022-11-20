57d3d9d62ebc456ecbae68e98f732b85e8a4e9a2
luisherreralg <lherreralg@outlook.es>
2022-11-20 17:49:40 +0100

    🏷️ Update robot types

--- src/infrastructure/types/types.ts
+++ src/infrastructure/types/types.ts
@@ -1,14 +1,20 @@
 export type Robot = {
     _id: string;
     name: string;
+    image: string;
     speed: number;
     endurance: number;
     creationDate: string;
     __v: number;
 };
 
+export type RobotsResponse = {
+    robots: Array<Robot>;
+};
+
 export type ProtoRobot = {
     name: string;
+    image: string;
     speed: number;
     endurance: number;
     creationDate: string;
