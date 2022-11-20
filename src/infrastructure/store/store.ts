8d8a5ae3118965f2be70956d6942f72e8d5cc7cb
luisherreralg <lherreralg@outlook.es>
2022-11-20 14:00:53 +0100

    🚧 Updated store file

--- src/infrastructure/store/store.ts
+++ src/infrastructure/store/store.ts
@@ -1,3 +1,11 @@
-export function placeholder() {
-    //
-}
+import { configureStore } from '@reduxjs/toolkit';
+import { robotReducer } from '../reducers/robotReducer';
+
+export const appStore = configureStore({
+    reducer: {
+        robots: robotReducer,
+    },
+});
+
+export type rootStore = typeof appStore;
+export type rootState = ReturnType<typeof appStore.getState>;
